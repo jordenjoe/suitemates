00376f2f5486cd692ab33ed10bfd9b1b7a44c6e9
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-11-30 14:35:09 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import coverimg from './assets/images/coverimg.png';
+import coverdivider from './assets/images/divider.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='Cover-Image'><img src={coverimg}/></div>
+        <h1>Make suite management <span>easy</span></h1>
+        <p>Get rent reminders, create house rules,
+          request quiet times, and manage a schedule
+            with all your suitemates!</p>
+            <div className='Divider'><img src={coverdivider}/></div>
+          <button className='Turquoise-Button'>Get started</button>
     </div>
   );
 }
