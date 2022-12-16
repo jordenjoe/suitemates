29b486729ddbc83adf0263e0dbc3d9adfcb257fd
PupinIsBeautiful <60634766+PupinIsBeautiful@users.noreply.github.com>
2022-12-16 16:33:06 -0500

    Create ChoreName.js

---
+++ src/components/ChoreName.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function ChoreName() {
+	
+	return (
+		<form className = "ChoreName">
+
+			<input className="chore-name-input" type="text" id="name" placeholder="Enter Chore"></input>
+
+		</form>
+	);
+}
+
+export default ChoreName;
