02017f2c8e67e51b0d17e07149b1a1bbbce85c24
PupinIsBeautiful <60634766+PupinIsBeautiful@users.noreply.github.com>
2022-12-16 16:37:44 -0500

    Create Assigning.js
    Inputting name of Chore requesting

---
+++ src/components/Assigning.js
@@ -0,0 +1,12 @@
+import '../assets/css/Chores.css';
+import ChoreName from '../components/ChoreName';
+
+
+const Assigning = () =>{
+	return (
+		<div className="Assigning">
+			<div className="Header">Chore Name</div>
+			<ChoreName/>
+		</div>
+	);
+};
