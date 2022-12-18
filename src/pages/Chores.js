27f2fe0c6d194d0b831a7f986a3a47db09a362dd
Ava Kim <avakim@dyn-160-39-149-60.dyn.columbia.edu>
2022-12-18 01:03:15 -0500

    added the two chore blocks

--- src/pages/Chores.js
+++ src/pages/Chores.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import '../assets/css/Login.css';
-import '../assets/css/Home.css';
 import '../assets/css/Chores.css';
 import Navbar from "../components/NavbarChoresBlue";
 import statusbar from '../assets/images/statusbar.png';
 import ChoresHeader from '../assets/images/chores/ChoresHeader.png';
 import Chores1022Cal from '../assets/images/chores/Chores1022Cal.png';
+import BathroomIncomplete from '../assets/images/chores/BathroomIncomplete.png';
+import LoungeIncomplete from '../assets/images/chores/LoungeIncomplete.png';
 
 const Home = () => {
     return (
@@ -13,7 +13,9 @@ const Home = () => {
         <img alt='status bar for phone' src={statusbar}/>
         <img alt='chores header' src={ChoresHeader}/>
         <img alt='chores header, Oct 22' src={Chores1022Cal}/>
-        <h1>Chores</h1>
+        <h2>Chores To Do</h2>        
+        <img alt='POV chore incomplete' src={BathroomIncomplete}/>
+        <img alt='Suitemate chore incomplete' src={LoungeIncomplete}/>
         <Link to="/Home">
             <button className="Turquoise-Button">Assign Chore</button>
         </Link>
