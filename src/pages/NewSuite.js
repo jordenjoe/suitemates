bca3310b638599fbb313db3178b5f95e49cafc39
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-12-18 00:34:19 -0500

    more login updates

--- src/pages/NewSuite.js
+++ src/pages/NewSuite.js
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import React, {useState} from 'react';
 import '../assets/css/Login.css';
 import Suitemates from "../components/Suitemates";
 import statusbar from '../assets/images/statusbar.png';
 import invitecode from '../assets/images/login/invitecode.png';
 import buildingicon from '../assets/images/login/buildingicon.png';
+import PopUp from './CopyInviteCodePopup';
 
 
 
 const NewSuite = () => {
+    const [popUp, setPopUp] = useState(false)
+    // adds class to darken background color
+    const duringPopUp = popUp ? " during-popup" : ""
     return (
-    <div className="App">
+    <div className={"App" + duringPopUp}>
          <img alt='status bar for phone' src={statusbar}/>
          <img src={buildingicon}/>
          <Suitemates />
@@ -18,7 +23,8 @@ const NewSuite = () => {
          <Link to="/home">
             <div className='Turquoise-Button-Login'>Go to homepage</div>
          </Link>
-         <img src={invitecode}/>
+         <img onClick={()=>setPopUp(true)} src={invitecode}/>
+         {popUp && <PopUp setPopUp={setPopUp}/>}   
          </div>
          
 
