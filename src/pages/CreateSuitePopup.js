1aae08f9f1197696c52564a54631cfce8d1f0348
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-12-18 00:10:17 -0500

    additional login screens + popups

---
+++ src/pages/CreateSuitePopup.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// styling
+import '../assets/css/Login.css';
+// images
+import back from '../assets/images/back.png';
+import createsuite from '../assets/images/login/createsuite.png';
+import { Link } from "react-router-dom";
+
+
+
+const PopUp = props => {
+    // function that takes boolean as param to conditionally display popup
+    const { setPopUp } = props 
+
+    return (
+        <div className="PopUp">
+            
+            <div className="PopUp-Content">
+                <img onClick={()=> setPopUp(false)} src={createsuite}/>
+                <p><input className='Popup-Input' placeholder="Suite Name" type="text"></input>
+                <Link to="/newsuite">
+                    <button className="Popup-Plus">+</button>
+                </Link>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default PopUp;
--- src/pages/JoinSuite.js
+++ src/pages/JoinSuite.js
@@ -4,13 +4,17 @@ import Suitemates from "../components/Suitemates";
 import statusbar from '../assets/images/statusbar.png';
 import eye from '../assets/images/login/eye.png';
 import buildingicon from '../assets/images/login/buildingicon.png';
+import React, {useState} from 'react';
+import PopUp from './CreateSuitePopup';
 
 
-
-const Signup = () => {
+const JoinSuite = () => {
+    const [popUp, setPopUp] = useState(false)
+    // adds class to darken background color
+    const duringPopUp = popUp ? " during-popup" : ""
     return (
-      <div className="App">
-      <img alt='status bar for phone' src={statusbar}/>
+      <div className={"App" + duringPopUp}>
+      <img className={duringPopUp} alt='status bar for phone' src={statusbar}/>
       <img alt='status bar for phone' src={buildingicon}/>
       <Suitemates />
       <p><span className='Small-Gray-Text'>You are not currently in a suite.</span></p>
@@ -18,14 +22,15 @@ const Signup = () => {
       <div className='Align-Left'>
       <p><span className='Small-Gray-Text'>Enter your invite code 🎉</span></p>
       </div>
-      <div className='Gray-Button'>XHUISDH23S</div>
+      <input className='Gray-Input' placeholder="XHUISDH23S" type="text" id="fname" name="fname"></input>
       </div>
       <Link to="/home">
          <div className='Turquoise-Button-Login'>Join Suite</div>
       </Link>
-
+      <p><span className='Small-Gray-Text'>No suite yet? </span><span onClick={()=>setPopUp(true)} className={'Small-Turqoise-Text' + duringPopUp}>Create a new suite!</span></p>
+      {popUp && <PopUp setPopUp={setPopUp}/>}   
       </div>
     );
   };
   
-export default Signup;
+export default JoinSuite;
