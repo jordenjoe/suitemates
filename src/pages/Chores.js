aa6a07d540bcd4946b55d5407714fd2374621478
Ava Kim <avakim@dyn-160-39-149-60.dyn.columbia.edu>
2022-12-18 00:38:42 -0500

    Added images, test run.

--- src/pages/Chores.js
+++ src/pages/Chores.js
@@ -4,13 +4,15 @@ import '../assets/css/Home.css';
 import '../assets/css/Chores.css';
 import Navbar from "../components/NavbarChoresBlue";
 import statusbar from '../assets/images/statusbar.png';
-
-
+import ChoresHeader from '../assets/images/ChoresHeader.png';
+import Chores1022Cal from '../assets/images/Chores1022Cal.png';
 
 const Home = () => {
     return (
       <div className="App">
         <img alt='status bar for phone' src={statusbar}/>
+        <img alt='chores header' src={ChoresHeader}/>
+        <img alt='chores header, Oct 22' src={Chores1022Cal}/>
         <h1>Chores</h1>
         <Link to="/Home">
             <button className="Turquoise-Button">Assign Chore</button>
