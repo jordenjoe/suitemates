86743e7db41c3c094134d856f2a9742b8bed5f08
Josephine Chan <jcf1996chan@gmail.com>
2022-12-19 12:05:56 -0500

    Updated update rent modal and other details

--- src/pages/Rent.js
+++ src/pages/Rent.js
@@ -4,24 +4,76 @@ import "../assets/css/Rent.css";
 //Components
 import Navbar from "../components/NavbarRentBlue";
 import RentHeader from "../components/rent_components/RentHeader";
+import RentStatus from "../components/rent_components/RentStatus";
+import UpdateRent from "../components/rent_components/UpdateRent";
 //Images
 import statusbar from "../assets/images/statusbar.png";
-import oldRent from "../assets/images/rent/old-rent.png";
-import newRent from "../assets/images/rent/new-rent.png";
 import updateRentButton from "../assets/images/rent/update-rent-button.png";
 import nudgeButton from "../assets/images/rent/nudge-button.png";
-import nudgeRoommate from "../assets/images/rent/nudge-roommate.png";
 import optOut from "../assets/images/rent/opt-out-rent.png";
 
 const Home = () => {
+  //State variables
+  const [modalActive, setModalActive] = useState(false);
+  const [nudgeModalActive, setNudgeModalActive] = useState(false);
+  const [updateModalActive, setUpdateModalActive] = useState(false);
+  //1 = original, 2 = modified, 3 = paid
+  const [rentStatus, setRentStatus] = useState(1);
+
+  //Functions
+  const showNudgeModal = () => {
+    setModalActive(true);
+    setNudgeModalActive(true);
+  };
+
+  const dismissNudgeModal = () => {
+    setModalActive(false);
+    setNudgeModalActive(false);
+  };
+
+  const showUpdateModal = () => {
+    setModalActive(true);
+    setUpdateModalActive(true);
+  };
+
+  const dismissUpdateModal = () => {
+    setModalActive(false);
+    setUpdateModalActive(false);
+  };
+
+  const confirmUpdateModal = () => {
+    setRentStatus(2);
+  };
+
   return (
     <div className="App">
+      <span className={modalActive ? "modal-background" : ""}>
+        {updateModalActive && (
+          <div className="modal-container">
+            <UpdateRent
+              onDismiss={dismissUpdateModal}
+              onConfirm={confirmUpdateModal}
+            />
+          </div>
+        )}
+        {nudgeModalActive && <div className="modal-container">hey</div>}
+      </span>
+
       <img alt="status bar for phone" src={statusbar} />
       <RentHeader />
-      <img src={oldRent} alt="rent before modifying it" />
-      <img src={updateRentButton} alt="update rent button" />
-      <img src={nudgeButton} alt="nudge roommate button" />
-      <img src={optOut} alt="opt out notifications" />
+
+      <RentStatus rentStatus={rentStatus} setRentStatus={setRentStatus} />
+
+      <div className="rent-buttons-container">
+        <button className="update-rent-button" onClick={showUpdateModal}>
+          <img src={updateRentButton} alt="update rent button" />
+        </button>
+        <button className="nudge-button" onClick={showNudgeModal}>
+          <img src={nudgeButton} alt="nudge roommate button" />
+        </button>
+        <img src={optOut} alt="opt out notifications" />
+      </div>
+
       <Navbar />
     </div>
   );
