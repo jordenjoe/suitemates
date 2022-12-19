e062324632e5892b650eb8d178bd652b36b6aa59
Josephine Chan <jcf1996chan@gmail.com>
2022-12-19 10:59:12 -0500

    Updated basic components to rent

--- src/pages/Rent.js
+++ src/pages/Rent.js
@@ -1,19 +1,30 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../assets/css/Login.css';
-import '../assets/css/Home.css';
+import "../assets/css/Rent.css";
+//Components
 import Navbar from "../components/NavbarRentBlue";
-import statusbar from '../assets/images/statusbar.png';
-
-
+import RentHeader from "../components/rent_components/RentHeader";
+//Images
+import statusbar from "../assets/images/statusbar.png";
+import oldRent from "../assets/images/rent/old-rent.png";
+import newRent from "../assets/images/rent/new-rent.png";
+import updateRentButton from "../assets/images/rent/update-rent-button.png";
+import nudgeButton from "../assets/images/rent/nudge-button.png";
+import nudgeRoommate from "../assets/images/rent/nudge-roommate.png";
+import optOut from "../assets/images/rent/opt-out-rent.png";
 
 const Home = () => {
-    return (
-      <div className="App">
-        <img alt='status bar for phone' src={statusbar}/>
-        <h1>Rent stuff</h1>
-       <Navbar />
-      </div>
-    );
-  };
-  
+  return (
+    <div className="App">
+      <img alt="status bar for phone" src={statusbar} />
+      <RentHeader />
+      <img src={oldRent} alt="rent before modifying it" />
+      <img src={updateRentButton} alt="update rent button" />
+      <img src={nudgeButton} alt="nudge roommate button" />
+      <img src={optOut} alt="opt out notifications" />
+      <Navbar />
+    </div>
+  );
+};
+
 export default Home;
