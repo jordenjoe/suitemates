0886e5503158c28c3abe864cf326b3187a3ed79a
Josephine Chan <jcf1996chan@gmail.com>
2022-12-20 21:10:46 -0500

    Updating and clearning up chores layout

--- src/components/AssignedPopUp.js
+++ src/components/AssignedPopUp.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import'../assets/css/Chores.css';
-import jess from '../assets/images/chores/Jess.png';
-import {Link} from "react-router-dom";
-import okbutton from '../assets/images/chores/okbutton.png';
-import AssignButton from '../assets/images/chores/AssignButton.png';
+import React from "react";
+import "../assets/css/Chores.css";
+import jess from "../assets/images/chores/Jess.png";
+import { Link } from "react-router-dom";
+import okbutton from "../assets/images/chores/okbutton.png";
+import AssignButton from "../assets/images/chores/AssignButton.png";
 
-const AssignedPopUp = props => {
-    const {setPopUp} = props
+const AssignedPopUp = (props) => {
+  const { onDismiss } = props;
 
-    return(
-        <div className="PopUp-Chores">
-            <div className="PopUpContent-Chores">
-                <h1>SUCCESS</h1>
-                <p>Jess has been asked to Clean Table by 10/25/2022</p>
-                <div className="PopUpIcons-Chores"><img alt='jess' src={jess}/></div>
-                <div className="PopUpIcons-Chores">
-                    <Link to="/NewChoreAssigned">
-                        <img alt='ok' src={okbutton}/>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="PopUp-Chores">
+      <div className="PopUpContent-Chores">
+        <h1>SUCCESS</h1>
+        <p>Jess has been asked to Clean Table by 10/25/2022</p>
+        <div className="PopUpIcons-Chores">
+          <img alt="jess" src={jess} />
         </div>
-    );
-
+        <div className="PopUpIcons-Chores">
+          <Link to="/NewChoreAssigned">
+            <img alt="ok" src={okbutton} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AssignedPopUp;
--- src/pages/AssignChore.js
+++ src/pages/AssignChore.js
@@ -1,40 +1,52 @@
-import {Link} from "react-router-dom";
-import '../assets/css/Login.css';
-import '../assets/css/Chores.css';
-import CloseButton from '../assets/images/chores/CloseButton.png';
-import EmptyBigCal from '../assets/images/chores/EmptyBigCal.png';
-import statusbar from '../assets/images/statusbar.png';
+import { Link } from "react-router-dom";
+import "../assets/css/Login.css";
+import "../assets/css/Chores.css";
+import CloseButton from "../assets/images/chores/CloseButton.png";
+import EmptyBigCal from "../assets/images/chores/EmptyBigCal.png";
+import statusbar from "../assets/images/statusbar.png";
 import Navbar from "../components/NavbarChoresBlue";
-import AssignButton from '../assets/images/chores/AssignButton.png';
-import React,{useState} from 'react';
-import PopUp from '../components/AssignedPopUp';
+import AssignButton from "../assets/images/chores/AssignButton.png";
+import React from "react";
 
 const AssignChore = () => {
-    const [popUp,setPopUp] = useState(false)
-    const duringPopUp=popUp ? " durind-popup" : ""
-    return(
-        <div className="App">
-            <img alt='status bar for phone' src={statusbar}/>
-            <div className="Single-Icon-Header">
-                <div className="Single-Icon-Header-Font">Assign Chore</div>
-                <Link to="/Chores">
-                    <img className="Close_Icon" alt='Close button' src={CloseButton}/>
-                </Link>
-            </div>
-            <p>Chore Name</p>
-            <input className='Gray-Input-Chores' placeholder="Enter Chore" type="text" id="fname" name="fname"></input>
-            <p>Assign To</p>
-            <input className='Gray-Input-Chores' placeholder="Enter SuiteMate" type="text" id="fname" name="fname"></input>
-            <p>Complete By</p>
-            <Link to="/AssignChoreCalUpdate">
-                <img alt='Chore Calendar Big' src={EmptyBigCal}/>
-            </Link>
-            <img onClick={()=> setPopUp(true)} alt='assign button' src={AssignButton}/>
-            {popUp&& <PopUp setPopUp={setPopUp}/>}
-            <div className="NavPadding-Assign"><Navbar/></div>
-        </div>
-    );
-
+  return (
+    <div className="App">
+      <img alt="status bar for phone" src={statusbar} />
+      <div className="Single-Icon-Header">
+        <div className="Single-Icon-Header-Font">Assign Chore</div>
+        <Link to="/Chores">
+          <img className="Close_Icon" alt="Close button" src={CloseButton} />
+        </Link>
+      </div>
+      <p>Chore Name</p>
+      <input
+        className="Gray-Input-Chores"
+        placeholder="Enter Chore"
+        type="text"
+        id="fname"
+        name="fname"
+      ></input>
+      <p>Assign To</p>
+      <input
+        className="Gray-Input-Chores"
+        placeholder="Enter SuiteMate"
+        type="text"
+        id="fname"
+        name="fname"
+      ></input>
+      <p>Complete By</p>
+      <Link to="/AssignChoreCalUpdate">
+        <img alt="Chore Calendar Big" src={EmptyBigCal} />
+      </Link>
+      <img
+        alt="assign button cannot be pressed until you finish filling the form"
+        src={AssignButton}
+      />
+      <div className="NavPadding-Assign">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default AssignChore;
